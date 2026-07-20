fac748752ac31ef9b025ce1aa9a0486dfe1639ea
Pankhuri22-P <pankhurip30@gmail.com>
2026-07-20 14:05:39 +0200

    removed one recommendation card AbandonedKinesisStreams from GUI

--- ngui/ui/src/hooks/useOptscaleRecommendations.ts
+++ ngui/ui/src/hooks/useOptscaleRecommendations.ts
@@ -1,7 +1,7 @@
 import { useMemo } from "react";
 import AbandonedImages from "containers/RecommendationsOverviewContainer/recommendations/AbandonedImages";
 import AbandonedInstances from "containers/RecommendationsOverviewContainer/recommendations/AbandonedInstances";
-import AbandonedKinesisStreams from "containers/RecommendationsOverviewContainer/recommendations/AbandonedKinesisStreams";
+// import AbandonedKinesisStreams from "containers/RecommendationsOverviewContainer/recommendations/AbandonedKinesisStreams";
 import AbandonedLoadBalancers from "containers/RecommendationsOverviewContainer/recommendations/AbandonedLoadBalancers";
 import AbandonedNebiusS3Buckets from "containers/RecommendationsOverviewContainer/recommendations/AbandonedNebiusS3Buckets";
 import AbandonedS3Buckets from "containers/RecommendationsOverviewContainer/recommendations/AbandonedS3Buckets";
